1f77fa7e21b6457bca44771f795d0c01ba3269f2
Radhika Rao <67799531+radhika757@users.noreply.github.com>
2024-12-16 10:37:33 +0530

    Search result populated

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,22 +10,49 @@ import SearchResults from "@/components/SearchResults";
 
 export default function Home() {
   const [isSearching, setIsSearching] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState([]);
 
-  const handleSearchFocus = () => {
+  const handleSearch = async (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+
+    if (query.trim() === "") {
+      setIsSearching(false);
+      setResults([]);
+      return;
+    }
+
     setIsSearching(true);
+    setLoading(true);
+
+    try {
+      const res = await fetch(`/api/search?search=${query}`);
+      const data = await res.json();
+
+      if (data.success) {
+        setResults(data.data);
+      } else {
+        setResults([]);
+      }
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+      setResults([]);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div>
       <Navbar />
-
       {/* Content */}
       <div
         className={`flex flex-col items-center ${isSearching ? '' : 'gap-20'} transition-all duration-300 ${isSearching ? "mt-[120px]" : "mt-auto justify-center h-screen"
           }`}
         style={{ height: isSearching ? "auto" : "calc(100vh - 110px)" }}
       >
-        {/* Logo */}
         {!isSearching && (
           <div className="flex items-center justify-start gap-10 w-[800px]">
             <Image
@@ -37,12 +64,10 @@ export default function Home() {
           </div>
         )}
 
-        {/* Search Bar */}
-        <SearchBar isSearching={isSearching} handleSearch={handleSearchFocus}/>
+        <SearchBar isSearching={isSearching} handleSearch={handleSearch} />
 
-        {/* Div below search bar */}
         {isSearching && (
-          <SearchResults />
+          <SearchResults results={results} />
         )}
       </div>
     </div>
